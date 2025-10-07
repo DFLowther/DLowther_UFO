6446bccde864d5c2424511ca4487624f0bbf0dd1
DFLowther <Duncan.froggy@gmail.com>
2025-10-07 15:27:49 -0500

    Altered Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputActionReference move;
+    public InputActionReference grow;
     private Rigidbody2D rigidbody2D;
     public float speed = 100.0f;
     public float maxSpeed = 100.0f;
+    public float growthSpeed = 3.0f;
+    public float shrinkSpeed = 1.0f;
+    public Vector3 targetScale = Vector3.one;
+
+    void OnEnable() 
+    {
+        grow.action.started += Grow;
+    }
+
+    void OnDisable()
+    {
+        grow.action.started -= Grow;
+    }
 
     void Start()
     {
@@ -14,13 +30,32 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        Vector2 inputDir = move.action.ReadValue<Vector2>();
 
-        //Debug.Log("x: "  + h + "y: " + v);
-
-        Vector2 dir = new Vector2(h, v).normalized;
+        Vector2 dir = inputDir.normalized;
         rigidbody2D.AddForce(dir * speed);
         rigidbody2D.linearVelocity = Vector3.ClampMagnitude(rigidbody2D.linearVelocity, maxSpeed);
     }
+
+    void Grow(InputAction.CallbackContext _context)
+    {
+        targetScale = new Vector3(2.0f, 2.0f, 2.0f);
+    }
+
+    void Update()
+    {
+        if (transform.localScale.x < targetScale.x)
+        {
+            transform.localScale = new Vector3(transform.localScale.x + growthSpeed * Time.deltaTime,
+                                               transform.localScale.y + growthSpeed * Time.deltaTime,
+                                               transform.localScale.z + growthSpeed * Time.deltaTime);
+
+            if (transform.localScale.x >= targetScale.x)
+            {
+                targetScale = Vector3.one;
+            }
+        }    
+    }
+
+
 }
